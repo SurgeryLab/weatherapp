77774154dfc6c6a13e8226ca7b59c2177aea181c
SurgeryLab <SurgeryLab@users.noreply.github.com>
2019-12-06 06:12:46 +0300

    Updates

---
+++ precache-manifest.6716c61287fa6fe5c51268c359d91cf7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df87cc154c2b5cf7f518e2a2523eb8d1",
+    "url": "/weatherapp/index.html"
+  },
+  {
+    "revision": "b2a3ebf24668b152a53a",
+    "url": "/weatherapp/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "bdafe8684230b29fac29",
+    "url": "/weatherapp/static/css/main.2135afcf.chunk.css"
+  },
+  {
+    "revision": "b2a3ebf24668b152a53a",
+    "url": "/weatherapp/static/js/2.98960107.chunk.js"
+  },
+  {
+    "revision": "bdafe8684230b29fac29",
+    "url": "/weatherapp/static/js/main.69b76acd.chunk.js"
+  },
+  {
+    "revision": "542a5abfc10a1944d74a",
+    "url": "/weatherapp/static/js/runtime-main.ff8156bb.js"
+  },
+  {
+    "revision": "cc54c19b29890841267ee0750eb25a00",
+    "url": "/weatherapp/static/media/polygon.cc54c19b.jpg"
+  }
+]);
